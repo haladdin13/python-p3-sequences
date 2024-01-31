cb169d69becff856d8714df7826ecdd4f4fe0a16
Hussein Aladdin <haladdin13@outlook.com>
2024-01-31 10:01:36 -0500

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print('[]')
+        return
+    fibonacci_sequence = [0, 1]
+    for i in range(2, length):
+        next_number = fibonacci_sequence[i-1] + fibonacci_sequence[i-2]
+        fibonacci_sequence.append(next_number)
+    print(str(fibonacci_sequence[:length]))
+
+# Example usage
+print_fibonacci(10)
